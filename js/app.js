2326881c808b15c796507f4ce352b579d372db64
nicolekarpo <84873240+nicolekarpo@users.noreply.github.com>
2022-06-01 17:56:56 +0200

    publish at github

--- js/app.js
+++ js/app.js
@@ -2,88 +2,84 @@ import { cdnUrl, projectID } from './env.js';
 import { handleImage, handleParagraphs } from './utils.js';
 
 function init() {
-    const urlString = window.location.search;
-    const paramsUrl = new URLSearchParams(urlString);
-    const pageValue = paramsUrl.get('page')
+const URL = window.location.href;
+console.log(URL)
+const urlString = window.location.search;
+const paramsUrl = new URLSearchParams(urlString);
+const pageValue = paramsUrl.get('page')
 
+// se nærmere på den over^^
+const burgerIcon = document.querySelector('.burger-icon');
+const mobileNav = document.querySelector('.mobile-nav');
+burgerIcon.addEventListener('click', () => {
+mobileNav.classList.toggle('mobile-nav-hide');
+burgerIcon.classList.toggle('burger');
+burgerIcon.classList.toggle('closemobilemenu');
+});
 
-    const burgerIcon = document.querySelector('.burger-icon');
-    const mobileNav = document.querySelector('.mobile-nav');
-    burgerIcon.addEventListener('click', () => {
-        mobileNav.classList.toggle('mobile-nav-hide');
-        burgerIcon.classList.toggle('burger');
-        burgerIcon.classList.toggle('closemobilemenu');
-    });
-
-    if(pageValue === null) {
-        getPosts(); 
-    } else {
-        getPost(pageValue);
-    }
+if(pageValue === null) {
+getPosts(); 
+} else {
+getPost(pageValue);
+}
 }
 
 
 async function getPost(pageValue) {
-    const project = document.querySelector('.project');
-    const post = await fetch(`https://06l51xgj.api.sanity.io/v1/data/query/production?query=*)
-    [slug.current == "${pageValue}"]
-    `);
-    const { result } = await post.json();
-   
-    project.append(handleImage(result[0].mainImage.asset._ref));
-    const title = document.createElement('h1');
-    title.innerText = result[0].title;
-    project.append(title)
-    project.append(handleParagraphs(result[0].body));
+const project = document.querySelector('.project');
+const post = await fetch(`https://06l51xgj.api.sanity.io/v1/data/query/production?query=*[slug.current == "${pageValue}"]`);
+const { result } = await post.json();
+
+project.append(handleImage(result[0].mainImage.asset._ref));
+const title = document.createElement('h1');
+title.innerText = result[0].title;
+project.append(title)
+project.append(handleParagraphs(result[0].body));
 }
 
 
+
 async function getPosts() {
-    
-    const posts =  await fetch(`https://06l51xgj.api.sanity.io/v1/data/query/production?query=*`);
-    ['_type' == 'post']
-   
-    const { result } = await posts.json();
 
-    console.log(result)
-   
-    
+const posts = await fetch(`https://06l51xgj.api.sanity.io/v1/data/query/production?query=*`);
+['_type' == "post"]
 
-    
-    const worksList = document.querySelector('.workslist');
+const { result } = await posts.json();
 
-    const cdnUrl  = 'https://cdn.sanity.io/images/06l51xgj/production/';
+const worksList = document.querySelector('.workslist');
 
-    
-    result.forEach(post => {
-       
-        const workBlock = document.createElement('a'); 
-        workBlock.classList.add('work'); 
-        workBlock.setAttribute(
-            'href', 
-            `./work.html?page=${post.slug.current}`
-        ); 
-        
-        
-        const workTitle = document.createElement('h2'); 
-        workTitle.classList.add('work-title'); 
-        workTitle.innerText = post.title; 
-        workBlock.append(workTitle); 
-        const workMask = document.createElement('div'); 
-        workMask.classList.add('work-mask'); 
-        workBlock.append(workMask); 
+const cdnUrl = 'https://cdn.sanity.io/images/my37wf9g/production/';
 
-        const workCover = document.createElement('img'); 
-       
-        const cover = post.mainImage.asset._ref.split('-'); 
-       
-        workCover.classList.add('work-cover');
-        workCover.setAttribute('src', `${cdnUrl}${cover[1]}-${cover[2]}.${cover[3]}`);
+result.forEach(post => {
+//
+const workBlock = document.createElement('a'); 
+workBlock.classList.add('work'); 
+if(post.slug) {
+workBlock.setAttribute(
+'href', `./work.html?page=${post.slug.current}`
+); 
+};
 
- workBlock.append(handleImage(post.mainImage.asset._ref, 'work-cover'));
-         worksList.append(workBlock); 
-         console.log(worksList)
-    });
+//
+const workTitle = document.createElement('h2'); 
+workTitle.classList.add('work-title'); 
+workTitle.innerText = post.title; 
+workBlock.append(workTitle); 
+const workMask = document.createElement('div'); 
+workMask.classList.add('work-mask'); 
+workBlock.append(workMask); 
+//bildehåndtering 
+const workCover = document.createElement('img'); 
+
+let cover = ''
+if(post.mainImage) {
+cover = post.mainImage.asset._ref.split('-'); 
+workCover.classList.add('work-cover'); 
+workCover.setAttribute('src',`${cdnUrl}${cover[1]}-${cover[2]}.${cover[3]}`);
+workBlock.append(handleImage(post.mainImage.asset._ref, 'work-cover'));
+worksList.append(workBlock); 
+}
+});
 
 }
 
